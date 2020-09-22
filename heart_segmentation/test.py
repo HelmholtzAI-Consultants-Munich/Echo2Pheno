fixed dice score computation

heart_segmentation/test.py
@@ -14,7 +14,7 @@ from matplotlib import pyplot as plt
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.join(SCRIPT_DIR, PACKAGE_PARENT))
-from heart_segmentation.utils import dice_confusion_matrix
+from heart_segmentation.utils import dice_score_binary
 import heart_segmentation.constants as constants
 from heart_segmentation.quicknat import QuickNat
 from heart_segmentation.dataset import BasicDataset, ResizeNpy
@@ -195,7 +195,7 @@ def predict_all(net, device, test_loader, save_dir):
             mask_pred = torch.round(mask_pred)  
             # calculate evalutation metrics    
             mse_eval += mse(mask, mask_pred)
-            dice_avg, _ = dice_confusion_matrix(mask_pred, mask, 1)
+            dice_avg = dice_score_binary(mask_pred, mask)
             dice_eval += dice_avg
             # save the first 10 images
             if idx<10:

heart_segmentation/trainer.py
@@ -9,7 +9,7 @@ from torchvision.transforms import RandomAffine, Compose, Resize, ToTensor, Rand
 from tensorboardX import SummaryWriter
 from datetime import datetime
 
-from utils import dice_confusion_matrix
+from utils import dice_score_binary
 from dataset import BasicDataset
 
 class Trainer(object):
@@ -253,7 +253,7 @@ class Trainer(object):
             # calculate evaluation metrics
             mask_pred = torch.round(mask_pred)
             mse_eval += self.mse(mask, mask_pred)
-            dice_avg, _ = dice_confusion_matrix(mask_pred, mask, 1)
+            dice_avg = dice_score_binary(mask_pred, mask) 
             dice_eval += dice_avg
 
             self.global_step += 1
@@ -307,7 +307,7 @@ class Trainer(object):
             # compute mse and dice score
             mask_pred = torch.round(mask_pred)
             avg_mse += self.mse(mask, mask_pred).item()
-            dice_score, _ = dice_confusion_matrix(mask_pred, mask, 1)
+            dice_score = dice_score_binary(mask_pred, mask) 
             avg_dice += dice_score
         # every 2 epochs save image and masks to tensorboard
         if epoch % 2 == 0: